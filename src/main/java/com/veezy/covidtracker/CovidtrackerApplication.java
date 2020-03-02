ddc25a307f3cfd51dde086c686db9aedd16c0514
veezyjay <viktorbasi545@gmail.com>
2020-03-02 14:24:59 +0100

    Initial project setup

---
+++ src/main/java/com/veezy/covidtracker/CovidtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.veezy.covidtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidtrackerApplication.class, args);
+    }
+
+}
